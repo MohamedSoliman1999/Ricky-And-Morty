89906196171fe1ac0710997b0cc67f48be78fb95
Mohamed Soliman <mohamedismail001122@yahoo.com>
2022-02-13 22:42:14 +0200

    kotlin DSL management script

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,23 @@
+object DependencyVersions {
+    const val coreKtxVersion = "1.7.0"
+}
+
+object Dependency {
+    const val coreKTX = "androidx.core:core-ktx:${DependencyVersions.coreKtxVersion}"
+}
+
+object ConfigVersions {
+
+}
+
+object Config {
+
+}
+
+object PluginVersions {
+
+}
+
+object Plugins {
+
+}
